908bf5ec66135c5dc50913576b168b238f10f737
Won Woo Lyu <wlyu125@gmail.com>
2023-06-14 15:24:00 +0900

    one done

---
+++ 530-Minimum_Absolute_Difference_in_BST/code.py
@@ -0,0 +1,22 @@
+class Solution:
+    def __init__(self):
+        self.vals = list()
+        self.min_val = 10**6
+
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.traverse(root)
+        self.vals.sort()
+        
+        for l, r in zip(self.vals, self.vals[1:]):
+            self.min_val = min(self.min_val, abs(l - r))
+
+        return self.min_val
+
+    def traverse(self, node):
+        if node is None:
+            return
+        
+        self.vals.append(node.val)
+        
+        self.traverse(node.left)
+        self.traverse(node.right)
