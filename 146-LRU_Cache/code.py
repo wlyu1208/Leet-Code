bb69d3f3671ff6ebee14551c7a0107adefbd1508
Won Woo Lyu <wlyu125@gmail.com>
2023-07-18 17:38:17 +0900

    medium | python version

---
+++ 146-LRU_Cache/code.py
@@ -0,0 +1,30 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        val = self.cache.pop(key)
+        self.cache[key] = val
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        
+        if len(self.cache) == self.capacity:
+            self.cache.popitem(last=False)
+        
+        self.cache[key] = value
+
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
