f928805de3d1af84d12cf1308c09569cadb3c883
Won Woo Lyu <wlyu125@gmail.com>
2023-08-23 17:27:17 +0900

    Time: 40 ms (65.48%), Space: 16.3 MB (28.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,8 +7,8 @@ class Solution:
     def helper(self, n, memo):
         if n == 0 or n == 1:
             return 1
-
+        
         if n not in memo:
-            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+            memo[n] = self.helper(n - 1, memo) + self.helper(n - 2, memo)
         
         return memo[n]
