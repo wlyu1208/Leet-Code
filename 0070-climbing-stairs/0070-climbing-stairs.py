07ed369f5590c4357cb64fefa138204b57dd6643
Won Woo Lyu <wlyu125@gmail.com>
2023-08-23 13:20:43 +0900

    Time: 36 ms (83.9%), Space: 16.3 MB (28.4%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        return self.helper(n, memo)
+    
+    def helper(self, n, memo):
+        if n == 0 or n == 1:
+            return 1
+
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        
+        return memo[n]
