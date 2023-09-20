feb0b58e4ab13c15aeefa91eb569148a9ef9a6ff
Won Woo Lyu <wlyu125@gmail.com>
2023-09-20 18:19:31 +0900

    Time: 59 ms (74.09%), Space: 20.3 MB (73.46%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,19 +2,23 @@ class MinStack:
 
     def __init__(self):
         self.array = []
-        
+        self.min_array = []
 
     def push(self, val: int) -> None:
         self.array.append(val)
+        if self.min_array:
+            val = min(self.min_array[-1], val)
+        self.min_array.append(val)
 
     def pop(self) -> None:
         self.array.pop()
+        self.min_array.pop()
 
     def top(self) -> int:
         return self.array[-1]
 
     def getMin(self) -> int:
-        return min(self.array)
+        return self.min_array[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
