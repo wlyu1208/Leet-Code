8c4c1133b48cdd715dbe90a2fd505fe825d046ed
Won Woo Lyu <wlyu125@gmail.com>
2023-12-12 15:57:22 +0900

    Time: 4 ms (63.28%), Space: 43.5 MB (71.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,11 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for (int i=0; i < n - 1; i++) {
-            for (int j=i+1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                     return new int[] {i, j};
-                }
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i=0; i<nums.length; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        for (int i=0; i<nums.length; i++) {
+            int rest = target - nums[i];
+            if (numMap.containsKey(rest) && numMap.get(rest) != i) {
+                return new int[] {i, numMap.get(rest)};
             }
         }
         return new int[] {};
