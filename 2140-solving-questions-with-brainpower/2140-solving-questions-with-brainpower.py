c3dc65b4db1156db80ea93591517cda37a732315
Won Woo Lyu <wlyu125@gmail.com>
2025-04-01 19:50:57 +0900

    Time: 132 ms (45.2%), Space: 66.5 MB (38.08%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,16 +1,21 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         n = len(questions)
+        cache = [0] * n
 
         def find_max(i):
             if i >= n:
                 return 0
+
+            if cache[i] != 0:
+                return cache[i]
             
             point, brain_power = questions[i]
 
-            return max(
+            cache[i] = max(
                 find_max(i+1),
                 point + find_max(i + 1 + brain_power)
             )
+            return cache[i]
         
         return find_max(0)
