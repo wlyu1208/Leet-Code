9cb34388ffdcdda9509b26b96a130ce11005b150
Won Woo Lyu <wlyu125@gmail.com>
2025-05-10 11:39:10 +0900

    Time: 0 ms (100%), Space: 17.9 MB (25.57%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        def gcd(len1, len2):
+            while len2:
+                len1, len2 = len2, len1 % len2
+            return len1
+
+        return str1[:gcd(len(str1), len(str2))]
