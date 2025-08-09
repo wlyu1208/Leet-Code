930b306cc7e45b083764aae22d069b4004a159fd
Won Woo Lyu <wlyu125@gmail.com>
2025-08-09 18:03:47 +0900

    Time: 0 ms (100%), Space: 17.8 MB (34.18%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,9 +1,13 @@
-import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        # if n <= 0: return False
-        # elif n == 1: return True
-        # elif math.log2(n).is_integer(): return True
-        # return False
+        if n <= 0:
+            return False
+        
+        binaries = bin(n)
+        bins = binaries[2:]
+        cnt_1 = bins.count('1')
 
-        return n > 0 and n & (n-1) == 0
+        if cnt_1 > 1:
+            return False
+        
+        return True
