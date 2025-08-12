1068dbfaa4745eb7adbae9c9661f40778890bfb1
Won Woo Lyu <wlyu125@gmail.com>
2025-08-12 18:25:50 +0900

    Time: 0 ms (100%), Space: 17.9 MB (98.54%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h = 0
+        for i, c in enumerate(citations):
+            if c >= i + 1:
+                h = i+1
+            else:
+                break
+        return h
+            
