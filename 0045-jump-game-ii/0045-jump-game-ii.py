a73f011a58569a791856a8dcd262b15f6e43e65a
Won Woo Lyu <wlyu125@gmail.com>
2025-08-12 16:34:40 +0900

    Time: 9 ms (50.72%), Space: 18.7 MB (36.04%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        near = far = jump = 0
+        farthest = jump = cur_end = 0
 
-        while far < len(nums) - 1:
-            farthest = 0
-            for i in range(near, far+1):
-                farthest = max(farthest, i + nums[i])
-            
-            near = far + 1
-            far = farthest
-            jump += 1
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+
+            if i == cur_end:
+                jump += 1
+                cur_end = farthest
         return jump
